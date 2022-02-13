11bcd59ae86f72e27b88b60ca90fadac1edf7eb9
Volkov <mark.volkovgit@gmail.com>
2022-02-13 20:59:11 +0200

    create after merge

--- components/TESTING/index.tsx
+++ components/TESTING/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export default function TESTING() {
-    return <div>HELLO TESTING</div>;
+    return (
+        <>
+            <div>HELLO TESTING</div>
+            <div>add stash </div>
+        </>
+    );
 }
